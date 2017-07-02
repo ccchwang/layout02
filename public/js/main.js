c10cbc130012a65b773daa6f4f78258d78a16252
Chloe Hwang <c.chloeh@gmail.com>
2017-07-02 17:52:56 -0400

    got open mechanism working

--- public/js/main.js
+++ public/js/main.js
@@ -4,6 +4,10 @@ const selectElements = function (selector) {
   return [].slice.call(nodes)
 };
 
+const selectElement = function (selector) {
+  return document.getElementsByClassName(selector)[0]
+};
+
 const registerClick = function(item, i) {
   //find neighbors of the item
   const upperItems = menuItems.slice(0, i);
@@ -14,15 +18,34 @@ const registerClick = function(item, i) {
     item.style.top = `${item.offsetTop * -1}px`;
     item.className += ' opened';
 
+    //show close button
+    $closeBtn.className += ' show';
+
     upperItems.forEach(i => i.className += " move-up")
     lowerItems.forEach(i => i.className += " move-down")
   }
 
+  const closeItem = function() {
+    item.style.top = 0;
+    item.classList.remove("opened");
+
+    //hiden close button
+    $closeBtn.classList.remove("show");
+
+    upperItems.forEach(i => i.classList.remove("move-up"))
+    lowerItems.forEach(i => i.classList.remove("move-down"))
+  }
+  ///
+
   item.addEventListener('click', openItem)
+  $closeBtn.addEventListener('click', closeItem)
 }
 
-////
+//CACHED VARIABLES
+const $closeBtn = selectElement('close-btn');
+
+//FLOW
 
 const menuItems = selectElements('.menu__item');
 
-menuItems.forEach(registerClick)
+menuItems.forEach(registerClick);
