fad7ac8142a52aeab6b35ea17cc0133f206ca908
Chloe Hwang <c.chloeh@gmail.com>
2017-07-30 20:35:12 -0400

    set up development tasks for css

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,100 @@
 var gulp = require('gulp');
+
+//SASS + CSS
 var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+
+//SERVER
 var browserSync = require('browser-sync').create();
 
 
-gulp.task('compile-css', function(){
-  return gulp.src('assets/scss/style.scss')
-    .pipe(sass()) // Using gulp-sass
-    .pipe(gulp.dest('assets/css'))
+
+
+
+
+//RUN SERVER
+gulp.task('browserSync', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'public'
+    },
+  })
+})
+
+
+//COMPILE SASS
+gulp.task('compile-sass', function(){
+  return gulp.src('assets/stylesheets/style.scss')
+    .pipe(sass()) //compile sass to css
+    .pipe(postcss([ autoprefixer({browsers: ['last 3 versions']}) ])) //autoprefixer
+    .pipe(gulp.dest('public/stylesheets'))
     .pipe(browserSync.reload({
       stream: true
     }))
 });
 
-gulp.task('browserSync', function() {
-  browserSync.init({
-    server: {
-      baseDir: 'assets'
-    },
-  })
-})
 
-gulp.task('watch', ['browserSync', 'compile-css'], function(){
-  gulp.watch('assets/scss/**/*.scss', ['compile-css']);
+//DEVELOPMENT + PRODUCTION CSS
+  // return gulp.src(paths.src)
+  //   .pipe(gulpif(!global.production, sourcemaps.init()))
+  //   .pipe(sass(TASK_CONFIG.stylesheets.sass))
+  //   .pipe(autoprefixer(TASK_CONFIG.stylesheets.autoprefixer))
+  //   .pipe(gulpif(global.production, cssnano(cssnanoConfig)))
+  //   .pipe(gulpif(!global.production, sourcemaps.write()))
+  //   .pipe(gulp.dest(paths.dest))
+  //   .pipe(browserSync.stream())
+
+
+
+
+//WATCH
+gulp.task('watch', ['browserSync', 'compile-sass'], function(){
+  gulp.watch('assets/stylesheets/**/*.scss', ['compile-sass']);
 
   // Reloads the browser whenever HTML or JS files change
-  gulp.watch('app/*.html', browserSync.reload);
-  gulp.watch('app/js/**/*.js', browserSync.reload);
+  gulp.watch('public/*.html', browserSync.reload);
+  gulp.watch('assets/js/**/*.js', browserSync.reload);
 })
+
+
+
+
+//CSS
+  // DEVELOPMENT
+  // 1) Compile Sass to CSS - DONE
+  // 2) Autoprefix - DONE
+  // 3) Live reload on save of asset files - DONE
+
+  // PRODUCTION
+  // 1) Create source maps
+  // 2) Minify
+
+
+//JAVASCRIPT
+  // DEVELOPMENT
+  // 1) Bundle together all imports into single js file
+  // 2) Live reload on save of asset files
+
+  // PRODUCTION
+  // 1) Minify
+
+
+//HTML
+  // DEVELOPMENT
+  // 1) Live reload on save of asset files - DONE
+
+  // PRODUCTION
+  // 1) Minify
+
+
+//IMAGES
+  // PRODUCTION
+  // 1) Optimize
+
+
+//BUILD TASK FOR PROUDCTION
+// gulp.task('build', [`clean`, `sass`, `useref`, `images`, `fonts`], function (){
+//   console.log('Building files');
+// })
