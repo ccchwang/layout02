adadd23f8e2441e3a1070dfb05d63132e96f2106
Chloe Hwang <c.chloeh@gmail.com>
2017-07-31 14:30:30 -0400

    set up gulp tasks for HTML + JS

--- gulpfile.js
+++ gulpfile.js
@@ -6,8 +6,12 @@ var autoprefixer = require('gulp-autoprefixer');
 var cssnano = require('gulp-cssnano');
 var browserSync = require('browser-sync').create();
 var htmlmin = require('gulp-htmlmin');
+var webpack = require('webpack-stream');
+var uglify = require('gulp-uglify');
 
-
+const handleError = function(err){
+  console.error(err);
+}
 
 
 //RUN SERVER
@@ -20,11 +24,12 @@ gulp.task('browserSync', function() {
 })
 
 
-//DEVELOPMENT + PRODUCTION CSS
+//CSS
 gulp.task('css', function(){
   return gulp.src('assets/stylesheets/style.scss')
     .pipe(gulpif(!global.production, sourcemaps.init()))
     .pipe(sass())
+    .on('error', handleError)
     .pipe(autoprefixer({browsers: ['last 3 versions']}))
     .pipe(gulpif(global.production, cssnano({preset: 'default'})))
     .pipe(gulpif(!global.production, sourcemaps.write()))
@@ -42,46 +47,54 @@ gulp.task('html', function(){
 });
 
 
+//JAVASCRIPT
+gulp.task('javascript', function(){
+  return gulp.src('assets/js/main.js')
+    .pipe(webpack({
+      watch: global.production ? false : true,
+      module: {
+        loaders: [
+          {
+            test: /\.js$/,
+            loader: 'babel-loader',
+            query: {
+              presets: ['es2015']
+            }
+          }
+        ]
+      },
+      output: {
+        filename: 'bundle.js',
+      },
+    }))
+    .on('error', handleError)
+    .pipe(gulpif(global.production, uglify()))
+    .pipe(gulp.dest('public/js'))
+    .pipe(browserSync.stream())
+});
+
+
 
 //WATCH - DEVELOPMENT
-gulp.task('watch', ['browserSync', 'css', 'html'], function(){
+gulp.task('watch', ['css', 'html', 'javascript', 'browserSync'], function(){
   gulp.watch('assets/stylesheets/**/*.scss', ['css']);
 
   // Reloads the browser whenever HTML or JS files change
   gulp.watch('assets/*.html', ['html']);
-  gulp.watch('assets/js/**/*.js', browserSync.reload);
+
+})
+
+gulp.task('clean', function(){
+  return global.production = true;
 })
 
 
-
-//BUILD
-gulp.task('build', ['browserSync'], function(){
-  global.production = true;
-
-  gulp.watch('assets/stylesheets/**/*.scss', ['css']);
-
-  // Reloads the browser whenever HTML or JS files change
-  gulp.watch('assets/*.html', ['html']);
-  gulp.watch('assets/js/**/*.js', browserSync.reload);
+//BUILD - PRODUCTION
+gulp.task('build', ['clean', 'css', 'html', 'javascript'], function(){
+  console.log('Finished building!')
 })
 
 
-
-//JAVASCRIPT
-  // DEVELOPMENT
-  // 1) Bundle together all imports into single js file
-  // 2) Live reload on save of asset files
-
-  // PRODUCTION
-  // 1) Minify
-
-
-//HTML
-  // DEVELOPMENT
-  // 1) Live reload on save of asset files - DONE
-
-  // PRODUCTION
-  // 1) Minify
 
 
 //IMAGES
@@ -90,7 +103,7 @@ gulp.task('build', ['browserSync'], function(){
 
 
 
-  //CSS
+//CSS
   // DEVELOPMENT
   // 1) Compile Sass to CSS - DONE
   // 2) Autoprefix - DONE
@@ -99,6 +112,23 @@ gulp.task('build', ['browserSync'], function(){
 
   // PRODUCTION
   // 1) Minify -- DONE
+
+
+//HTML
+  // DEVELOPMENT
+  // 1) Live reload on save of asset files - DONE
+
+  // PRODUCTION
+  // 1) Minify - DONE
+
+
+//JAVASCRIPT
+  // DEVELOPMENT
+  // 1) Bundle together all imports into single js file - DONE
+  // 2) Live reload on save of asset files - DONE
+
+  // PRODUCTION
+  // 1) Minify - DONE
 
 
 
